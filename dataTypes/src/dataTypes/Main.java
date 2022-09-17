254d9112495dcdf1ab1b3bb4330c66c4bb5ab03e
ahmet-demir <113617682+ahmet-demir@users.noreply.github.com>
2022-09-17 22:34:31 +0300

    Add files via upload

---
+++ dataTypes/src/dataTypes/Main.java
@@ -0,0 +1,29 @@
+package dataTypes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	
+	
+
+
+			String mesaj1 =("mesaj yazdım");
+			int sayi = 124 ; //tam sayı değeri varilir
+			long sayi1 = 123456; // dayüksek veri alanı olan tam sayı 
+			double sayi2 = 1.8 ; // virgüllü sayısal ifadeler tutar
+			boolean evetHayır = false ; // true veya false olarak vari tutar
+			char karakter = 'a' ; // tek bir karakter tutar 
+			System.out.println(mesaj1);
+			System.out.println(sayi+sayi1);
+			System.out.println(sayi2);
+			System.out.println(evetHayır);
+			System.out.println(karakter);
+			
+		
+
+	}
+
+
+}
